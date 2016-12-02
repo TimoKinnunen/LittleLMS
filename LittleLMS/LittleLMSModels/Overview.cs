627ebd36d160be47c73ced90bab5a5b392458d76
anli79 <anders.b.lindgren@gmail.com>
2016-12-02 15:16:06 +0100

    Skapat en kursöversikt.

---
+++ LittleLMS/LittleLMSModels/Overview.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LittleLMS.LittleLMSModels {
+    public class Overview {
+        public int Id { get; set; }
+
+        [Display(Name = "Course name")]
+        public string CourseName { get; set; }
+
+        [Display(Name = "Description")]
+        public string CourseDescription { get; set; }
+
+        [Display(Name = "Course start date")]
+        public DateTime CourseStartDate { get; set; }
+
+        [Display(Name = "Module name")]
+        public string ModuleName { get; set; }
+
+        [Display(Name = "Description")]
+        public string ModuleDescription { get; set; }
+
+        [Display(Name = "Module start date")]
+        public DateTime ModuleStartDate { get; set; }
+
+        [Display(Name = "Module end date")]
+        public DateTime ModuleEndDate { get; set; }
+    }
+}
